d5031addda0c3e8e86431604d12bd87b13b91593
Laura <laura.buliga@iis.utm.md>
2024-05-30 19:03:54 +0300

    Update App.tsx

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,49 +1,14 @@
 import React, { useState } from 'react';
 import ModalForm from '../FormaAdaugare';
 
-import { Button, Layout, Menu, theme } from 'antd';
-import ProductCard from '../TelefonCard';
+import { Button, Layout, Menu, Spin, theme } from 'antd';
 import Telefon from '../models/TelefonAndroid';
 import MenuContainer from './Meniu';
+import telefonStore from '../stores/TelefonStore';
+import TelefonCard from '../TelefonCard';
 
 
 const { Header, Content, Footer, Sider } = Layout;
-
-
-
-const initialData:Telefon[] = [
-  {
-   nume: "Samsung Galaxy",
-   model: "S23",
-   descriere: "Display adaptiv Obține culoare și luminozitate optime, cu vizibilitate maximă în aer liber12. Ecranul de 120 Hz netezește derularea, în timp ce Eye Comfort Shield elimină oboseala ochilor, chiar și atunci când privești ecranul în întuneric.",
-   imagine:"https://images.samsung.com/is/image/samsung/p6pim/ro/2302/gallery/ro-galaxy-s23-s911-sm-s911bzkgeue-thumb-534700949?imwidth=480",
-   pret: 19000,
-   specificatii: "",
-   versiune: "10",
-   relizul: 2021
-
-  },
-  {
-    nume: "Xiaomi ",
-    model: "Redmi 13C",
-    descriere: "Telefon mobil Xiaomi Redmi 13C, 8GB RAM, 256GB, Midnight Black",
-    imagine:"https://s13emagst.akamaized.net/products/63471/63470391/images/res_2df52b48660b0fe2782fccf7b5d99f8a.jpg?width=720&height=720&hash=233E02D59EF1E27CECCDC4374FF6F310",
-    pret: 7500,
-    specificatii: "",
-    versiune: "10",
-    relizul: 2021
-   },
-   {
-    nume: "iPhone",
-    model: "15 Pro Max",
-    descriere: "Telefon mobil Apple iPhone 15 Pro Max, 256GB, 5G, Black Titanium",
-    imagine:"https://s13emagst.akamaized.net/products/60458/60457151/images/res_44ffbf30efb9dcbd1501e316af596edb.jpg?width=720&height=720&hash=F8B9B844788445D1E92A240F213B0D64",
-    pret: 22000,
-    specificatii: "",
-    versiune: "10",
-    relizul: 2021
-   }
-]; 
 
 
 const App: React.FC = () => {
@@ -52,26 +17,34 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [cardData, setCardData] = useState<Telefon[]>(initialData);
+  const [selectedProduct, setSelectedProduct] = React.useState<Telefon | null>(null); 
   const showModal = () => {
     setIsModalVisible(true);
   };
   const handleCancel = () => {
     
     setIsModalVisible(false);
-   
+    setSelectedProduct(null); 
+
   };
 
-  const handleFormSubmit = ( CardProduct: Telefon) => {
-    console.log("Handle submit")
-    console.log(CardProduct)
-    setCardData([...cardData, CardProduct]);
-    console.log("final card data");
-    console.log(cardData);
-
-    setIsModalVisible(false);
+  const handleFormSubmit = (telefonCard: Telefon) => {
+    if (selectedProduct) {
+      telefonStore.removeProduct(selectedProduct); 
+    }
+    telefonStore.addProduct(telefonCard); 
+    setIsModalVisible(false); 
+    setSelectedProduct(null); 
   };
 
+  const handleEdit = (telefon: Telefon) => {
+    setSelectedProduct(telefon); 
+    showModal();
+  };
+
+  const handleDelete = (telefon: Telefon) => {
+    telefonStore.removeProduct(telefon); 
+  };
 const emptyCard:Telefon = {
   nume : "",
   descriere: "",
@@ -84,7 +57,6 @@ const emptyCard:Telefon = {
 
 }
 
-console.log(cardData);
 
   return (
     <Layout hasSider>
@@ -99,7 +71,7 @@ console.log(cardData);
         <Button type="primary" onClick={showModal} style={{ marginBottom: 16, marginLeft:16}}>
             Add product
           </Button>
-          <ModalForm visible={isModalVisible} onCancel={handleCancel} onSubmit={handleFormSubmit} card = {emptyCard} />
+          <ModalForm visible={isModalVisible} onCancel={handleCancel} onSubmit={handleFormSubmit} card={selectedProduct || emptyCard} />
          
         </Header>
         <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
@@ -111,9 +83,20 @@ console.log(cardData);
               borderRadius: borderRadiusLG,
             }}
           >
-             <div style={{display:'flex',flexDirection:'row',gap:10 }}>{cardData.map((card , index) => (
-        <ProductCard key={index} telefon={card} />
-      ))}</div>
+              {telefonStore.loading ? (
+              <Spin size="large" />
+            ) : (
+              <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
+                {telefonStore.telefoane.map((telefon, index) => (
+                  <TelefonCard 
+                    key={index} 
+                    telefon={telefon} 
+                    onEdit={() => handleEdit(telefon)}
+                    onDelete={() => handleDelete(telefon)} 
+                  />
+                ))}
+              </div>
+            )}
 
 
           </div>
